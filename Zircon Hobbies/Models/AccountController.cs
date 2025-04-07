1bf57fd6412dc1a70a0068ac7bee036bbc34c7e3
Zach Affron <carolanbrian77@yahoo.ie>
2025-04-07 09:54:30 +0100

    Fixed Issue with Cart carrying accros various user sessoins

---
+++ Zircon Hobbies/Models/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+namespace Zircon_Hobbies.Models
+
+{
+    public class AccountController : Controller
+    {
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            HttpContext.Session.Clear();
+
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
